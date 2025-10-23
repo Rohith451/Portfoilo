0ff4be4f155b5dddad4af5c5968d5178b8fcfc02
Rohith451 <rohithonteru45@gmail.com>
2025-10-23 22:57:35 +0530

    Configure Vite base path and rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/'
+  // 🚨 Add this line, replacing 'Portfoilo' with your exact repository name
+  base: "/Portfoilo/", 
 })
